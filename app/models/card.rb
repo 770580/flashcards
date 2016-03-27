f8a3afb8edd59fa8c4c0d32cad0e2ffb0754ca9e
Anton <lascivolin@gmail.com>
2016-03-27 22:12:59 +0300

    add edit, delete cards

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,12 @@
 class Card < ActiveRecord::Base
+	validates :original_text,
+	          :translated_text,
+	          :review_date, presence: true
+	validate  :original_text_not_equal_translated_text
+
+	def original_text_not_equal_translated_text
+	  if original_text.mb_chars.downcase == translated_text.mb_chars.downcase
+	  	errors.add(:translated_text, "не должны быть равно оригинальному слову")
+	  end
+	end
 end
