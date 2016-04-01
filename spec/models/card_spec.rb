c3db6a408665f8e6759c2a64fe521d21fb682fe8
Anton <lascivolin@gmail.com>
2016-04-01 14:00:47 +0300

    final tests for card model with houndci

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
 describe Card do
-  before { @card = Card.new(original_text: "Дом", translated_text: "House",
-    						review_date: Date.today) }
+  before { @card = Card.new(original_text: "Дом", translated_text: "House", review_date: Date.today) }
 
   describe "when original_text and translated_text equal" do
     it "should be invalid" do
@@ -15,17 +14,17 @@ describe Card do
   end
 
   describe "get random card" do
-  	it "should be valid" do
-  	  @card.save
+    it "should be valid" do
+      @card.save
       @card = Card.random
       expect(@card).to be_valid
-  	end
+    end
   end
 
   describe "add 3 days in review_date" do
-  	it "should be valid" do
-  	  @card.inc_review_date
+    it "should be valid" do
+      @card.inc_review_date
       expect(@card.review_date).to eq(Date.today + 3.days)
-  	end
+    end
   end
 end
