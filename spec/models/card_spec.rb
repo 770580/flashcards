2c7fa9c78733db23a0ac71369e9d1997af37137d
Anton <lascivolin@gmail.com>
2016-04-01 14:41:34 +0300

    final tests for card model v.2 houndci

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Card do
-  let(:card){ Card.new(original_text: "Дом", translated_text: "House", review_date: Date.today) }
+  let(:card) { Card.new(original_text: "Дом", translated_text: "House", review_date: Date.today) }
 
   it "when original_text and translated_text equal should be invalid" do
     bad_words = %w[Дом ДоМ доМ]
