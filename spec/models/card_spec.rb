e0408dd12061eeb65c30f67febda1ab77ae46664
Anton <lascivolin@gmail.com>
2016-04-01 14:39:59 +0300

    final tests for card model v.2

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,30 +1,24 @@
 require 'rails_helper'
 
 describe Card do
-  before { @card = Card.new(original_text: "Дом", translated_text: "House", review_date: Date.today) }
+  let(:card){ Card.new(original_text: "Дом", translated_text: "House", review_date: Date.today) }
 
-  describe "when original_text and translated_text equal" do
-    it "should be invalid" do
-      bad_words = %w[Дом ДоМ доМ]
-      bad_words.each do |word|
-        @card.translated_text = word
-        expect(@card).not_to be_valid
-      end
+  it "when original_text and translated_text equal should be invalid" do
+    bad_words = %w[Дом ДоМ доМ]
+    bad_words.each do |word|
+      card.translated_text = word
+      expect(card).not_to be_valid
     end
   end
 
-  describe "get random card" do
-    it "should be valid" do
-      @card.save
-      @card = Card.random
-      expect(@card).to be_valid
-    end
+  it "random card should be exist" do
+    card.save
+    card_r = Card.random
+    expect(card).to eq(card_r)
   end
 
-  describe "add 3 days in review_date" do
-    it "should be valid" do
-      @card.inc_review_date
-      expect(@card.review_date).to eq(Date.today + 3.days)
-    end
+  it "add 3 days in review_date should be valid" do
+    card.inc_review_date
+    expect(card.review_date).to eq(Date.today + 3.days)
   end
 end
