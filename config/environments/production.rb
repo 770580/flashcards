603f1fb0908c8ed96269dea5e4090197405796c4
Anton <lascivolin@gmail.com>
2016-04-15 13:56:39 +0300

    some fixs smtp v.4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,10 +79,10 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'lit-plateau-35988.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    user_name:            ENV['GMAIL_USERNAME'],
-    password:             ENV['GMAIL_PASSWORD'],
+    address:              'smtp.mail.ru',
+    port:                 465,
+    user_name:            ENV['MAIL_USERNAME'],
+    password:             ENV['MAIL_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true  
   }
