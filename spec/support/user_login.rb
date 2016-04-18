0549939cf8efa3554a8c20a765dd4b3c19c85071
Anton <lascivolin@gmail.com>
2016-04-18 17:38:28 +0300

    add i18n to tests v.2

--- spec/support/user_login.rb
+++ spec/support/user_login.rb
@@ -1,7 +1,7 @@
 include ApplicationHelper
 def user_login(user, password = "asdf")
   visit login_path
-  fill_in "Email", with: user.email
-  fill_in "password", with: password
+  fill_in I18n.t('user_sessions.email'), with: user.email
+  fill_in I18n.t('user_sessions.pass_label'), with: password
   click_button I18n.t('user_sessions.sign_in')
 end
