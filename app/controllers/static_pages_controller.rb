3b7baf476f9c225f6e2195c7cec79df6197c39aa
Anton <lascivolin@gmail.com>
2016-04-27 10:52:52 +0300

    add ajax for check_form on index

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,5 +11,9 @@ class StaticPagesController < ApplicationController
     else
       @card = current_user.cards.random
     end
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end
