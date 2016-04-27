d163f4852204e56849703a3485fb664c50f07678
Anton <lascivolin@gmail.com>
2016-04-27 12:26:41 +0300

    add changes to static_pages_controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,18 +2,24 @@ class StaticPagesController < ApplicationController
   def index
     if !current_user.decks.first
       flash[:info] = I18n.t('static_pages.flash_need_deck')
-      return redirect_to decks_path
+      redirect_to decks_path
     elsif !current_user.cards.first
       flash[:info] = I18n.t('static_pages.flash_need_card')
-      return redirect_to new_card_path
+      redirect_to new_card_path
     elsif current_user.decks.choose_active.first
       @card = current_user.decks.choose_active.first.cards.random
+      responce_format
     else
       @card = current_user.cards.random
-    end
-    respond_to do |format|
-      format.html
-      format.js
+      responce_format
     end
   end
+
+  private
+    def responce_format
+      respond_to do |format|
+        format.html
+        format.js
+      end
+    end
 end
